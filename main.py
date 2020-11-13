2b576868b9126aaef958e4f2109edbaa2222cb9a
Wellington Farias <wellington.farias@sicoob.com.br>
2020-11-13 17:37:53 -0300

    Finalizado parte 1 do curso

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session, flash, url_for
+
 
 app = Flask(__name__)
+app.secret_key = 'dabliodc'
 
 
 class Jogo:
@@ -9,18 +11,82 @@ class Jogo:
         self.categoria = categoria
         self.console = console
 
-@app.route('/main')
-def iniciaAplication():
 
-    #Exemplo de String fixa
-    #lista = ['Wellington', 'Diego', 'Cardoso', 'Farias']
-    jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
-    jogo2 = Jogo('Metal Gear', 'Terror', 'PS1')
-    jogo3 = Jogo('Dragon Ball Heroes', 'Ação', 'PS4')
-    lista = [jogo1, jogo2, jogo3]
+class Usuario:
+    def __init__(self, id, nome, senha):
+        self.id = id
+        self.nome = nome
+        self.senha = senha
+
+
+usuario1 = Usuario('Wellington', 'Wellington Farias', '1234')
+usuario2 = Usuario('Luan', 'Luan Marques', '7a1')
+usuario3 = Usuario('flavio', 'Flávio', 'javascript')
+
+
+usuarios = { usuario1.id: usuario1,
+             usuario2.id: usuario2,
+             usuario3.id: usuario3 }
+
+
+# Exemplo de String fixa
+# lista = ['Wellington', 'Diego', 'Cardoso', 'Farias']
+jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
+jogo2 = Jogo('Metal Gear', 'Terror', 'PS1')
+jogo3 = Jogo('Dragon Ball Heroes', 'Ação', 'PS4')
+lista = [jogo1, jogo2, jogo3]
+
+
+@app.route('/')
+def index():
     return render_template('main.html',
-                           titulo='Pagina Inicial',
+                           titulo='Página Inicial',
                            jogos=lista)
 
-app.run()
 
+@app.route('/novo')
+def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima=url_for('novo')))
+    return render_template('novo.html', titulo='Novo jogo')
+
+
+@app.route('/criar', methods=['POST', ])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return redirect(url_for('index'))
+
+
+@app.route('/login')
+def login():
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
+
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if request.form['usuario'] in usuarios:
+        usuario = usuarios[request.form['usuario']]
+        if usuario.senha == request.form['senha']:
+            session['usuario_logado'] = usuario.id
+            flash(usuario.nome + ' logou com sucesso!')
+            proxima_pagina = request.form['proxima']
+            return redirect(proxima_pagina)
+
+    else :
+        flash('Não logado, tente de novo!')
+        return redirect(url_for('login'))
+
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Nenhum usuário logado!')
+    return redirect(url_for('index'))
+
+
+app.run(debug=True)
